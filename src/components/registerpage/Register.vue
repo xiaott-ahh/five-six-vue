7b6b1488cbb43b47775d626b0bb71c27eb5e9899
xiaott <13068729891@163.com>
2020-10-28 23:15:09 +0800

    注册界面开发

--- src/components/registerpage/Register.vue
+++ src/components/registerpage/Register.vue
@@ -1,17 +1,119 @@
 <template>
-  <div class="registerForm">
-    <login></login>
+  <div id="poster">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm"  class="register">
+      <h2 style="color: #3a91ba;">注册</h2>
+      <el-form-item label="用户名" prop="name">
+        <el-input size="medium" auto-complete="off" v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input size="medium" type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input size="medium" type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item style="margin-top: 20px;">
+        <el-button  type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button  @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-import Login from "@/components/LoginPage/Login"
 export default {
-  name: "Register",
-  components:{Login}
+  name: 'Register',
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass');
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error('两次输入密码不一致!'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        pass: '',
+        checkPass: '',
+        name: ''
+      },
+      rules: {
+        pass: [
+          { required: true, validator: validatePass, trigger: 'blur' }
+        ],
+        checkPass: [
+          { required: true, validator: validatePass2, trigger: 'blur' }
+        ],
+        name: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$axios.post('/register',{
+            name: this.ruleForm.name,
+            password: this.ruleForm.pass
+          }).then(resp => {
+            if (resp.data.code === 200) {
+              this.$alert('注册成功', '提示', {
+                confirmButtonText: '确定'
+              })
+              this.$router.replace('/login')
+            } else {
+              this.$alert(resp.data.message, '提示', {
+                confirmButtonText: '确定'
+              })
+            }
+            // eslint-disable-next-line no-unused-vars
+          }).catch(failResponse => {})
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
 }
 </script>
 
 <style scoped>
+
+#poster {
+  background: url("../../assets/images/login.jpg") no-repeat center;
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  position: fixed;
+}
+
+.register {
+  margin:100px auto;
+  height: 450px;
+  width: 300px;
+  padding: 10px 20px 0px 20px;
+  background: rgba(2, 10, 14, 0.5);
+}
+
+/deep/ .el-form-item__label {
+  color: white;
+}
 
 </style>
